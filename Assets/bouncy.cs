eec2fdf17bf99e3c2677ccca22a1616b1ceee1bb
stanlo <stanlographics@aol.com>
2018-03-24 02:13:07 -0700

    added bouncy cube

---
+++ Assets/bouncy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class bouncy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+  float s = 0f;
+  // Update is called once per frame
+  float y = 0f;
+	void Update () {
+    var samples = new float[256];
+    AudioListener.GetSpectrumData(samples, 0, FFTWindow.BlackmanHarris);
+    var x = samples.Max();
+    s = 0.1f * x  + 0.9f * s;
+    this.transform.localScale = Vector3.one * (1f + s* 100f);
+    y += s * 100f;
+    transform.localEulerAngles = new Vector3(y / 2f, y / 3f, y);
+
+  }
+}
