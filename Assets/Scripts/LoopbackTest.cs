396d3fae1636374701dc9454de9b8718b1ad60e9
Stanlo <stanlo2@hotmail.com>
2018-03-24 18:05:31 -0700

    User prefs for loopback script, cut plugin size to 6kb, added function to enumerate loopback input names -- hit space to cycle.

--- Assets/Scripts/LoopbackTest.cs
+++ Assets/Scripts/LoopbackTest.cs
@@ -5,44 +5,97 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 public class LoopbackTest : MonoBehaviour {
 
-  int i = 0;
-  void PCMReaderCallback(float[] data)
-  {
-  //  for (var j = 0; j < data.Length; ++j)
-  //  { 
-  //    data[j] = Mathf.Sin(i++ / 44100f * 2f * Mathf.PI * 440f);
+  private const string PLAYER_PREFS_DEVICE_INDEX = "LOOPBACK_DEVICE_INDEX";
+  private uint deviceIndex;
+  private int errorToneIndex = 0;
+  private string selectedEndpointLabelText;
 
-  //  }
+  void Start() {
+    deviceIndex = (uint)PlayerPrefs.GetInt(PLAYER_PREFS_DEVICE_INDEX, 0);
 
+    var endpointName = GetEndpointName(deviceIndex);
+    if (null != endpointName) {
+      selectedEndpointLabelText = String.Format("Using \"{0}\" as audio source.", endpointName);
+      if (!StartLoopback()) {
+        Debug.LogErrorFormat("Error using {0}!", endpointName);
+      }
+    } else {
+      Debug.LogError("Couldn't find a suitable audio source!");
+    }
+  }
+
+
+  void Update() {
+    var audioSource = GetComponent<AudioSource>();
+
+    if (Input.GetKeyDown(KeyCode.Space)) {
+      ++deviceIndex;
+
+      var endpointName = GetEndpointName(deviceIndex);
+      if (null == endpointName) {
+        deviceIndex = 0;
+        endpointName = GetEndpointName(deviceIndex);
+      }
+
+      if (null != endpointName) {
+        selectedEndpointLabelText = String.Format("Using \"{0}\" as audio source.", endpointName);
+        if (!StartLoopback()) {
+          Debug.LogErrorFormat("Error using {0}!", endpointName);
+        } else {
+          PlayerPrefs.SetInt(PLAYER_PREFS_DEVICE_INDEX, (int)deviceIndex);
+        }
+      } else {
+        Debug.LogError("Couldn't find a suitable audio source!");
+      }
+    }
+
+  }
+
+  private void OnGUI() {
+    if (null != selectedEndpointLabelText) {
+      GUI.Label(Screen.safeArea, selectedEndpointLabelText);
+    }
+  }
+
+  private void PCMReaderCallback(float[] data) {
     GCHandle pinnedArray = GCHandle.Alloc(data, GCHandleType.Pinned);
     IntPtr pointer = pinnedArray.AddrOfPinnedObject();
-    NativeApi.Read((uint)data.Length, pointer);
-
+    if (!NativeApi.Read((uint)data.Length, pointer)) {
+      for (var j = 0; j < data.Length; ++j) {
+        data[j] = Mathf.Sin((errorToneIndex++ / 2) / 44100f * 2f * Mathf.PI * 440f) * 0.15f;
+      }
+    }
     pinnedArray.Free();
-
-
   }
 
-  // Use this for initialization
-  void Start () {
-    NativeApi.Start(2u);
+  private bool StartLoopback() {
+    if (!NativeApi.Start(deviceIndex)) return false;
+
     var audioSource = GetComponent<AudioSource>();
-    audioSource.clip = AudioClip.Create("loopback", int.MaxValue, 1, 44100, true, PCMReaderCallback);
+    audioSource.clip = AudioClip.Create("loopback", int.MaxValue, 2, 44100, true, PCMReaderCallback);
     audioSource.Play();
-  }
-  
-  // Update is called once per frame
-  void Update () {
-    var audioSource = GetComponent<AudioSource>();
 
+    return true;
   }
 
-  struct NativeApi
-  {
+  private static string GetEndpointName(uint index) {
+    var namePtr = NativeApi.GetEndpointName(index);
+    if (IntPtr.Zero == namePtr) return null;
+
+    var nameString = Marshal.PtrToStringBSTR(namePtr);
+    Marshal.FreeBSTR(namePtr);
+
+    return nameString;
+  }
+
+  struct NativeApi {
     [DllImport("loopback_recorder")]
-    public static extern void Start(uint device_index);
+    public static extern bool Start(uint device_index);
 
     [DllImport("loopback_recorder")]
-    public static extern void Read(uint sample_count, IntPtr out_samples);
+    public static extern bool Read(uint sample_count, IntPtr out_samples);
+
+    [DllImport("loopback_recorder")]
+    public static extern IntPtr GetEndpointName(uint index);
   }
 }
--- loopback_recorder/loopback_recorder.cpp
+++ loopback_recorder/loopback_recorder.cpp
@@ -1,11 +1,8 @@
+#include <intrin.h>
+
 #include <atomic>
 #include <chrono>
-#include <condition_variable>
 #include <cstdio>
-#include <memory>
-#include <mutex>
-#include <thread>
-#include <vector>
 
 #include <Audioclient.h>
 #include <Mmdeviceapi.h>
@@ -17,26 +14,35 @@
 #define UNITY_INTERFACE_API __stdcall
 #define UNITY_INTERFACE_EXPORT __declspec(dllexport)
 
-#define __CHK(x, ret)                                                     \
-  {                                                                       \
-    auto result = x;                                                      \
-    if (FAILED(result)) {                                                 \
-      printf("%s(%d): error \"%s\" returned by %s\n", __FILE__, __LINE__, \
-             _com_error{result}.ErrorMessage(), #x);                      \
-      return ret;                                                         \
-    }                                                                     \
+#if _DEBUG
+#define LOG(...) printf(__VA_ARGS__)
+#else
+#define LOG(...)
+#endif
+
+#define __CHK(x, ret)                                                  \
+  {                                                                    \
+    auto result = x;                                                   \
+    if (FAILED(result)) {                                              \
+      LOG("%s(%d): error \"%s\" returned by %s\n", __FILE__, __LINE__, \
+          _com_error{result}.ErrorMessage(), #x);                      \
+      return ret;                                                      \
+    }                                                                  \
   }
 #define CHK(x) __CHK(x, /**/)
 #define CHK_NULL(x) __CHK(x, nullptr)
+#define CHK_FALSE(x) __CHK(x, false)
+#define CHK_ERR(x) __CHK(x, 1)
 
-static constexpr auto kMaxBufferSize = 1000000u;
+static constexpr auto kMaxBufferDelay = std::chrono::milliseconds(420);
+static constexpr auto kSampleBufferSize = 1000000u;
 static constexpr auto kSamplesPerSecond = 44100u;
 static constexpr WAVEFORMATEX kOutputFormat{
     WAVE_FORMAT_IEEE_FLOAT,
-    1u,
+    2u,
     kSamplesPerSecond,
-    kSamplesPerSecond * sizeof(float),
-    sizeof(float),
+    kSamplesPerSecond * 2 * sizeof(float),
+    sizeof(float) * 2,
     sizeof(float) * 8,
     0u,
 };
@@ -44,114 +50,167 @@ static constexpr WAVEFORMATEX kOutputFormat{
 template <typename T>
 class AutoPtr {
  public:
+  AutoPtr(T* value) : value_(value) {}
+  AutoPtr() = default;
+  AutoPtr(const AutoPtr&) = delete;
+  AutoPtr(AutoPtr&& other) { *this = std::move(other); }
+  void operator=(const AutoPtr&) = delete;
+  void operator=(AutoPtr&& rhs) {
+    if (value_) value_->Release();
+    value_ = rhs.value_;
+    rhs.value_ = nullptr;
+  }
   ~AutoPtr() {
     if (value_) value_->Release();
   }
   T** operator&() { return &value_; }
   T* operator->() { return value_; }
   operator bool() { return !!value_; }
+  T* Detach() {
+    auto old_value = value_;
+    value_ = nullptr;
+    return old_value;
+  }
 
  private:
   T* value_ = nullptr;
 };
 
-AutoPtr<IMMDeviceEnumerator> device_enumerator;
-std::vector<float> samples;
-std::mutex samples_mutex;
-std::condition_variable samples_condition_variable;
+IMMDeviceEnumerator* device_enumerator;
+float samples[kSampleBufferSize];
+uint32_t sample_count;
+CRITICAL_SECTION samples_critical_section;
+CONDITION_VARIABLE samples_condition_variable;
 std::atomic_bool quit;
-std::thread thread;
+HANDLE thread;
 
-void ThreadProc(uint32_t device_index) {
-  if (!device_enumerator) return;
-
-  AutoPtr<IMMDevice> device;
-
-  if (device_index == ~0u) {
-    CHK(device_enumerator->GetDefaultAudioEndpoint(
-        EDataFlow::eRender, ERole::eMultimedia, &device));
-  } else {
-    AutoPtr<IMMDeviceCollection> device_collection;
-    CHK(device_enumerator->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE,
-                                              &device_collection));
-
-    UINT device_count;
-    CHK(device_collection->GetCount(&device_count));
-
-    if (device_index >= (int)device_count) return;
-
-    CHK(device_collection->Item(device_index, &device));
-  }
+DWORD WINAPI ThreadProc(LPVOID parameter) {
+  AutoPtr<IMMDevice> device{(IMMDevice*)parameter};
 
   AutoPtr<IAudioClient> audio_client;
-  CHK(device->Activate(__uuidof(audio_client), CLSCTX_ALL, nullptr,
-                       (void**)&audio_client));
+  CHK_ERR(device->Activate(__uuidof(audio_client), CLSCTX_ALL, nullptr,
+                           (void**)&audio_client));
 
-  CHK(audio_client->Initialize(
+  CHK_ERR(audio_client->Initialize(
       AUDCLNT_SHAREMODE_SHARED,
       AUDCLNT_STREAMFLAGS_LOOPBACK | AUDCLNT_STREAMFLAGS_AUTOCONVERTPCM, 1, 0,
       &kOutputFormat, nullptr));
 
   AutoPtr<IAudioCaptureClient> capture_client;
-  CHK(audio_client->GetService(__uuidof(IAudioCaptureClient),
-                               (void**)&capture_client));
+  CHK_ERR(audio_client->GetService(__uuidof(IAudioCaptureClient),
+                                   (void**)&capture_client));
 
-  CHK(audio_client->Start());
+  CHK_ERR(audio_client->Start());
 
   while (!quit) {
     UINT32 packet_size;
-    CHK(capture_client->GetNextPacketSize(&packet_size));
+    CHK_ERR(capture_client->GetNextPacketSize(&packet_size));
 
     while (packet_size) {
-      float* sample_data;
-      UINT32 frames_read;
+      float* frame_data;
+      UINT32 num_frames_read;
       DWORD flags;
-      CHK(capture_client->GetBuffer((BYTE**)&sample_data, &frames_read, &flags,
-                                    nullptr, nullptr));
+      CHK_ERR(capture_client->GetBuffer((BYTE**)&frame_data, &num_frames_read,
+                                        &flags, nullptr, nullptr));
 
       {
-        std::lock_guard<std::mutex> lock{samples_mutex};
-        samples.insert(samples.end(), sample_data, sample_data + frames_read);
+        EnterCriticalSection(&samples_critical_section);
 
-        if (samples.size() > kMaxBufferSize) {
-          auto num_to_remove =
-              samples.size() - kMaxBufferSize + kMaxBufferSize / 2;
-          samples.erase(samples.begin(), samples.begin() + num_to_remove);
+        auto num_samples_read = num_frames_read * 2;
+        if (sample_count + num_samples_read >= kSampleBufferSize) {
+          sample_count = 0;
         }
+
+        __movsd((unsigned long*)(samples + sample_count),
+                (unsigned long*)frame_data, num_samples_read);
+        sample_count += num_samples_read;
+
+        LeaveCriticalSection(&samples_critical_section);
       }
-      samples_condition_variable.notify_all();
 
-      CHK(capture_client->ReleaseBuffer(frames_read));
+      WakeConditionVariable(&samples_condition_variable);
 
-      CHK(capture_client->GetNextPacketSize(&packet_size));
+      CHK_ERR(capture_client->ReleaseBuffer(num_frames_read));
+
+      CHK_ERR(capture_client->GetNextPacketSize(&packet_size));
     }
   }
+
+  return 0;
 }
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API Stop() {
   quit = true;
-  if (thread.joinable()) thread.join();
-  samples.clear();
+  if (thread) {
+    WaitForSingleObject(thread, INFINITE);
+    CloseHandle(thread);
+    thread = nullptr;
+  }
+  sample_count = 0;
 }
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+extern "C" bool UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
 Start(uint32_t device_index) {
   Stop();
   quit = false;
-  thread = std::thread{ThreadProc, device_index};
+
+  if (!device_enumerator) return false;
+
+  AutoPtr<IMMDevice> device;
+  if (device_index == ~0u) {
+    CHK_FALSE(device_enumerator->GetDefaultAudioEndpoint(
+        EDataFlow::eRender, ERole::eMultimedia, &device));
+  } else {
+    AutoPtr<IMMDeviceCollection> device_collection;
+    CHK_FALSE(device_enumerator->EnumAudioEndpoints(
+        eRender, DEVICE_STATE_ACTIVE, &device_collection));
+
+    UINT device_count;
+    CHK_FALSE(device_collection->GetCount(&device_count));
+
+    if (device_index >= (int)device_count) return false;
+
+    CHK_FALSE(device_collection->Item(device_index, &device));
+  }
+
+  thread = CreateThread(nullptr, 0, &ThreadProc, device.Detach(), 0, nullptr);
+  return true;
 }
 
-extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
-Read(uint32_t sample_count, float* out_samples) {
-  std::unique_lock<std::mutex> lock{samples_mutex};
-  auto wait_result = samples_condition_variable.wait_for(
-      lock, std::chrono::milliseconds{1000},
-      [&]() { return quit || sample_count <= samples.size(); });
-  if (!wait_result) return;
+extern "C" bool UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+Read(uint32_t out_sample_count, float* out_samples) {
+  if (!thread) return false;
 
-  memcpy(out_samples, samples.data(), sample_count * sizeof(float));
+  EnterCriticalSection(&samples_critical_section);
+  auto result = [&]() {
+    while (!quit && sample_count < out_sample_count) {
+      auto buffer_delay_ms =
+          (DWORD)std::chrono::duration_cast<std::chrono::milliseconds>(
+              kMaxBufferDelay)
+              .count();
 
-  samples.erase(samples.begin(), samples.begin() + sample_count);
+      auto sleep_result =
+          SleepConditionVariableCS(&samples_condition_variable,
+                                   &samples_critical_section, buffer_delay_ms);
+
+      if (!sleep_result && GetLastError() == ERROR_TIMEOUT) {
+        return false;
+      }
+    }
+
+    __movsd((unsigned long*)out_samples, (unsigned long*)samples,
+            out_sample_count);
+
+    __movsd((unsigned long*)samples,
+            (unsigned long*)(samples + out_sample_count),
+            sample_count - out_sample_count);
+    sample_count -= out_sample_count;
+
+    return true;
+  }();
+  LeaveCriticalSection(&samples_critical_section);
+
+  return result;
 }
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
@@ -160,39 +219,46 @@ UnityPluginLoad(void* /*unityInterfaces*/) {
   CHK(CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL,
                        IID_PPV_ARGS(&device_enumerator)));
 
-  Start(~0u);
+  InitializeCriticalSection(&samples_critical_section);
+  InitializeConditionVariable(&samples_condition_variable);
 }
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginUnload() {
   Stop();
+
+  device_enumerator->Release();
+  device_enumerator = nullptr;
+
+  DeleteCriticalSection(&samples_critical_section);
   CoUninitialize();
 }
 
-// void GetEndpoints() {
-//  if (!unmanaged_->device_enumerator) return nullptr;
-//
-//  AutoPtr<IMMDeviceCollection> device_collection;
-//  CHK_NULL(unmanaged_->device_enumerator->EnumAudioEndpoints(
-//      eRender, DEVICE_STATE_ACTIVE, &device_collection));
-//
-//  UINT device_count;
-//  CHK_NULL(device_collection->GetCount(&device_count));
-//
-//  auto name_list = gcnew List<String ^>();
-//  for (auto i = 0u; i < device_count; ++i) {
-//    AutoPtr<IMMDevice> device;
-//    CHK_NULL(device_collection->Item(i, &device));
-//
-//    AutoPtr<IPropertyStore> property_store;
-//    CHK_NULL(device->OpenPropertyStore(STGM_READ, &property_store));
-//
-//    PROPVARIANT property_variant;
-//    CHK_NULL(
-//        property_store->GetValue(PKEY_Device_FriendlyName,
-//        &property_variant));
-//
-//    name_list->Add(gcnew String(property_variant.bstrVal));
-//  }
-//
-//  return name_list;
-//}
+extern "C" BSTR UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
+GetEndpointName(UINT index) {
+  if (!device_enumerator) return nullptr;
+
+  AutoPtr<IMMDeviceCollection> device_collection;
+  CHK_NULL(device_enumerator->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE,
+                                                 &device_collection));
+
+  UINT endpoint_count;
+  CHK_NULL(device_collection->GetCount(&endpoint_count));
+  if (index >= endpoint_count) return nullptr;
+
+  AutoPtr<IMMDevice> device;
+  CHK_NULL(device_collection->Item(index, &device));
+
+  AutoPtr<IPropertyStore> property_store;
+  CHK_NULL(device->OpenPropertyStore(STGM_READ, &property_store));
+
+  PROPVARIANT property_variant;
+  CHK_NULL(
+      property_store->GetValue(PKEY_Device_FriendlyName, &property_variant));
+
+  return SysAllocString(property_variant.bstrVal);
+}
+
+BOOL WINAPI main(HINSTANCE /*hinstDLL*/, DWORD /*fdwReason*/,
+                 LPVOID /*lpvReserved*/) {
+  return TRUE;
+}
