dfb204f2a75805c652aaadd854b2b81def0c165e
Stanlo <stanlo2@hotmail.com>
2018-03-28 22:48:47 -0700

    update lines

--- Assets/LinesEffect.cs
+++ Assets/LinesEffect.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Linq;
 using UnityEngine;
 
 public class LinesEffect : MonoBehaviour {
@@ -8,8 +9,13 @@ public class LinesEffect : MonoBehaviour {
 
   public AudioSource audioSource;
   public Material material;
+  public float totalScaler = 1.0f;
+  public float totalDamp = 1.0f;
   private Texture2D[] audioTextures = new Texture2D[2];
   private float[] intensities = new float[SAMPLE_COUNT];
+  private float[] frequencies = new float[SAMPLE_COUNT];
+  private float prevIntensity;
+  private float[] beats = new float[4];
 
   private void Start() {
     audioTextures[0] = new Texture2D(SAMPLE_COUNT, 1, TextureFormat.RFloat, false);
@@ -17,22 +23,39 @@ public class LinesEffect : MonoBehaviour {
 
   }
 
-  int x = 0;
   private void Update() {
     material.SetTexture("intensities0", audioTextures[0]);
     material.SetTexture("intensities1", audioTextures[1]);
     material.SetFloat("sampleCount", SAMPLE_COUNT);
-    ++x;
-    for (int i = 0; i < 2; ++i) {
-      if ((x + i) % 2 != 0) continue;
-      audioSource.GetOutputData(intensities, i);
-      var intensitiesAddr = GCHandle.Alloc(intensities, GCHandleType.Pinned);
-      var pointer = intensitiesAddr.AddrOfPinnedObject();
-      audioTextures[i].LoadRawTextureData(pointer, SAMPLE_COUNT * sizeof(float));
-      audioTextures[i].Apply();
 
-      intensitiesAddr.Free();
-    }
+    var channelIndex = Time.frameCount % 2;
+
+    audioSource.GetOutputData(intensities, channelIndex);
+    var intensitiesAddr = GCHandle.Alloc(intensities, GCHandleType.Pinned);
+    var pointer = intensitiesAddr.AddrOfPinnedObject();
+    audioTextures[channelIndex].LoadRawTextureData(pointer, SAMPLE_COUNT * sizeof(float));
+    audioTextures[channelIndex].Apply();
+    intensitiesAddr.Free();
+
+    var total = intensities.Select(o => Mathf.Abs(o)).Sum();
+    total = totalDamp * total + (1 - totalDamp) * prevIntensity;
+    material.SetFloat("totalIntensity", totalScaler * Mathf.Log(1f + total) / SAMPLE_COUNT);
+    prevIntensity = total;
+
+    audioSource.GetSpectrumData(frequencies, channelIndex, FFTWindow.Blackman);
+    var b = frequencies.Take(frequencies.Length / 4).Sum();
+    beats[0] = Mathf.Max(beats[0] * 0.975f, b);
+
+    b = frequencies.Skip(frequencies.Length / 4).Take(frequencies.Length / 4).Sum();
+    beats[1] = Mathf.Max(beats[1] * 0.975f, b);
+
+    b = frequencies.Skip(2*frequencies.Length / 4).Take(frequencies.Length / 4).Sum();
+    beats[2] = Mathf.Max(beats[2] * 0.975f, b);
+
+    b = frequencies.Skip(3*frequencies.Length / 4).Take(frequencies.Length / 4).Sum();
+    beats[3] = Mathf.Max(beats[3] * 0.975f, b);
+    material.SetFloatArray("beats", beats);
+
   }
 
   private void OnRenderImage(RenderTexture src, RenderTexture dest) {
--- Assets/Materials/Lines.shader
+++ Assets/Materials/Lines.shader
@@ -1,10 +1,10 @@
-﻿Shader "Hidden/Lines"
+Shader "Hidden/Lines"
 {
   Properties
   {
     _MainTex("Texture", 2D) = "white" {}
     f("f", Float) = 25.0
-    lineBoost("lineBoost", Float) = 1.0
+      lineBoost("lineBoost", Float) = 1.0
   }
   SubShader
   {
@@ -45,9 +45,13 @@
       float sampleCount = 256;
       float f;
       float lineBoost;
+      float totalIntensity;
+      float beats[4];
 
       float F(float t) {
-        return exp(-t*f*.5);
+        return min(1 / (t*f*2.5), 15);
+
+        return  exp(-t * f*.5);
       }
 
 
@@ -55,12 +59,7 @@
       {
         return a + b * cos(6.28318*(c*t + d));
       }
-      float3 filmicToneMapping(float3 color)
-      {
-        color = max(0, color - 0.004);
-        color = (color * (6.2 * color + .5)) / (color * (6.2 * color + 1.7) + 0.06);
-        return color;
-      }
+
       float Sample(sampler2D s, float x) {
         float f = 2.0 * tex2D(s, float2(x + 0.0 / sampleCount, 0.5)).r 
           + tex2D(s, float2(x - 1.0 / sampleCount, 0.5)).r
@@ -71,20 +70,19 @@
       fixed4 frag (v2f i) : SV_Target
       {
         float inverseSampleCount = 1.0 / sampleCount;
-      float a = saturate(0.5 - abs(0.5 - i.uv.x)) / 0.5;
-       a = .7 * pow(smoothstep(0.1, 0.9, a), .5);
+        float a = saturate(0.5 - abs(0.5 - i.uv.x)) / 0.5;
+        a = .7 * pow(smoothstep(0.1, 0.9, a), .5);
         float intensity0 = a*Sample(intensities0, lerp(0.5 * inverseSampleCount, 1.0 - 0.5 * inverseSampleCount, i.uv.x));
         float intensity1 = a*Sample(intensities1, lerp(0.5 * inverseSampleCount, 1.0 - 0.5 * inverseSampleCount, i.uv.x));
 
         float y = i.uv.y * 2.0 - 1.0;
 
-        
         float3 f = 0;
         f += pal(float3(0.5,0.5,0.5),float3(0.5,0.5,0.5),float3(1.0,1.0,1.0),float3(0.0,0.33,0.67), _Time.y + 0.5) * F(abs(y - intensity0));
         f += pal(float3(0.5,0.5,0.5),float3(0.5,0.5,0.5),float3(1.0,1.0,1.0),float3(0.0,0.33,0.67), _Time.y) * F(abs(y - intensity1));
         //f += pal(float3(0.5,0.5,0.5),float3(0.5,0.5,0.5),float3(1.0,1.0,1.0),float3(0.0,0.33,0.67), 0.5) * F(abs(y - intensity2));
         //f += pal(float3(0.5,0.5,0.5),float3(0.5,0.5,0.5),float3(1.0,1.0,1.0),float3(0.0,0.33,0.67), 0.75) * F(abs(y - intensity3));
-        f *= lineBoost;
+        f *= lineBoost * totalIntensity * 1.5;
         //f = filmicToneMapping(f * 10.0);
         //f /= 4.0;
         //f *= 0.5;
@@ -100,6 +98,8 @@
         v = smoothstep(0.05, .9, pow(v, 1.0));
         //return v;
         //return float4(f, 1);
+
+        
         return float4(v*f, 1);
       }
       ENDCG
