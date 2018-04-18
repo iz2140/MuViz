9280be8506aa5dc135a521658330d8bbe2df69ea
Iris Zhang <iz2140@columbia.edu>
2018-04-17 23:47:36 -0700

    Messing around with rotating planes

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    private const int ROTATE_SPEED_MULTIPLIER = 4;
+
+	// Use this for initialization
+	void Start () { 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.parent.name == "Plane1")
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * ROTATE_SPEED_MULTIPLIER);
+        }
+        else {
+            transform.Rotate(Vector3.back * Time.deltaTime * ROTATE_SPEED_MULTIPLIER);
+        }
+    }
+}
